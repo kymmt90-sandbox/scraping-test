116d0a91b59630a7408b1aad91af259581f1e65b
Kohei Yamamoto <kymmt90@gmail.com>
2016-01-20 22:30:56 +0900

    Get names and completion dates of recently read 40 books

--- scrape_bookmeter.rb
+++ scrape_bookmeter.rb
@@ -1,9 +1,11 @@
 require 'mechanize'
+require 'nokogiri'
 
 ROOT_URL   = 'http://bookmeter.com'
 USER_ID    = '104835'
 LOGIN_URL  = "#{ROOT_URL}/login"
 MYPAGE_URL = "#{ROOT_URL}/u/#{USER_ID}"
+BOOKLIST_URL = "#{ROOT_URL}/u/#{USER_ID}/booklist"
 
 if ARGV.size < 2
   warn 'need mail and password'
@@ -24,4 +26,25 @@ agent.get(LOGIN_URL) do |page|
   end.submit
 end
 
-puts agent.get(MYPAGE_URL).content.toutf8
+booklist_content = agent.get(BOOKLIST_URL).content.toutf8
+booklist_doc = Nokogiri::HTML(booklist_content)
+book_page_urls = []
+book_names = []
+booklist_doc.css('div.book_box_book_title > a').each do |node|
+  book_page_urls << node.attribute('href').text
+  book_names << node.text
+end
+
+dates = []
+book_page_urls.each do |url|
+  book_content = agent.get(ROOT_URL + url).content.toutf8
+  book_doc = Nokogiri::HTML(book_content)
+  year  = book_doc.at_css('select#read_date_y > option').attribute('value').text
+  month = book_doc.at_css('select#read_date_m > option').attribute('value').text
+  day   = book_doc.at_css('select#read_date_d > option').attribute('value').text
+  dates << [year.to_s, month.to_s, day.to_s].join('/')
+end
+
+book_names.zip(dates) do |name_date|
+  p name_date
+end
